15593b5cdee3f307d613573abd4eb8b048ec2603
Mike Carlo Gonzaga <kaosvioge@gmail.com>
2023-09-21 01:48:16 +0800

    add change in Contact page

--- src/components/layout/home/Contact/index.tsx
+++ src/components/layout/home/Contact/index.tsx
@@ -1,9 +1,20 @@
 import Bg from "../../../../assets/images/Untitled-bg-white.png";
-import facebookIcon from "../../../../assets/icons/facebook-icon-logo.png";
-import linkedinIcon from "../../../../assets/icons/linkedin-icon-logo.png";
-import twitterIcon from "../../../../assets/icons/twitter-icon-logo.png";
+import emailjs from "@emailjs/browser";
+// import facebookIcon from "../../../../assets/icons/facebook-icon-logo.png";
+// import linkedinIcon from "../../../../assets/icons/linkedin-icon-logo.png";
+// import twitterIcon from "../../../../assets/icons/twitter-icon-logo.png";
 
 const Contact = () => {
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    emailjs.sendForm(
+      "service_ooahi1r",
+      "template_705n4tf",
+      e.currentTarget,
+      "9wYy5Htm45PW3P0pd"
+    );
+  };
+
   return (
     <div
       id="contact"
@@ -19,8 +30,30 @@ const Contact = () => {
           you as soon as possible.
         </h3>
       </div>
-      <div className="w-full h-full p-10 text-center mt-6 md:mt-12">
-        <span className="text-lg font-semibold">
+      <div className="w-full h-full p-10 text-center mt-6 md:mt-10">
+        <form
+          className="w-96 flex flex-col bg-slate-200 my-0 mx-auto space-y-2 p-6 rounded-md"
+          onSubmit={sendEmail}
+        >
+          <label htmlFor="emailFrom">Email:</label>
+          <input
+            type="text"
+            name="email_from"
+            id="emailFrom"
+            className="border-none rounded-md p-2.5 resize-y outline-none box-content border-solid border-b-2 border-transparent"
+            placeholder="person@example.com"
+          />
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            id="message"
+            className="border-none rounded-md p-2.5 resize-y outline-none box-content border-solid border-b-2 border-transparent"
+          ></textarea>
+          <button type="submit" className="bg-sunglow-500 rounded-md p-2">
+            Send
+          </button>
+        </form>
+        {/* <span className="text-lg font-semibold">
           email: kaosvioge@gmail.com
         </span>
 
@@ -64,7 +97,7 @@ const Contact = () => {
               </span>
             </a>
           </li>
-        </ul>
+        </ul> */}
       </div>
     </div>
   );
